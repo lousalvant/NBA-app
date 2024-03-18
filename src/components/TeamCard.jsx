cd994ae9238d51541e3b1ac487cfcbd5659a6fdd
lousalvant <116595993+lousalvant@users.noreply.github.com>
2024-03-18 17:10:36 -0400

    fourth commit

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -6,7 +6,7 @@ const SummaryCard = () => {
 
   // Function to generate a random team ID from the given list
   const getRandomTeamId = () => {
-    const teamIds = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 37, 39, 40];
+    const teamIds = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 38, 40, 41];
     return teamIds[Math.floor(Math.random() * teamIds.length)];
   };
 
